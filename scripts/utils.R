ac8dc4a6dcc7560073ec1a4a28f0ec3aaca3641e
Carlos <carloshvronchi@gmail.com>
2022-07-25 20:55:41 +0200

    change forestplot to save the plots and add documention for the util functions. save also table with parameters obtained from the survival models

--- scripts/utils.R
+++ scripts/utils.R
@@ -4,6 +4,29 @@
 # Description: A set of helper functions that are reused across the different
 #   documents in order to decluter them.
 
+
+#' Violin plot of pathway score given cohort and subgroups
+#'
+#' @param sum_exp Summarized experiment object. Used to fetch the scores
+#'   that are saved on the col data slot
+#' @param which_score A string. Which pathway score to select and plot 
+#'   the violin plots.
+#' @param clinical_variable A string. With which clinical variable
+#'   to subgroup
+#' @param color_by A string. A clinical variable to color the dots.
+#' @param base_size An integer. Argument for ggplot2::theme_bw
+#' @param point_size An integer. Argument for ggplot2::geom_point
+#' @param title_plot. An integer Argument for ggplot2::labs
+#' @return A plot. 
+#' @examples
+#' \dontrun{
+#' plot_scores_vs_clinics(
+#'     sum_exp  = metabric,
+#'     which_score = "set_erpr", 
+#'     clinical_variable = "er_status",
+#'     color_by = "er_status"
+#' )
+#' }
 plot_scores_vs_clinics <- function(
     sum_exp, which_score, clinical_variable, color_by, 
     base_size = 10,
@@ -22,9 +45,28 @@ plot_scores_vs_clinics <- function(
     
 }
 
-
+#' Umap projection plot from the specified cohort
+#'
+#' @inheritParams plot_scores_vs_clinics
+#' @param cohort A string. Name of the cohort that is being passed. It is
+#'   used for the title.
+#' @param umap_projection A matrix. Output from uwot::umap.
+#' @param alpha_val A double. Specified transparency for ggplot2::geom_point.
+#' @return A plot. 
+#' @examples
+#' \dontrun{
+#' plot_umap(
+#'     sum_exp  = metabric,
+#'     cohort = "METABRIC",
+#'     color_by = "er_status",
+#'     umap_projection = umap_metabric
+#' )
+#' }
 plot_umap <- function(
-    dataset, umap_projection, name_dataset, color_by,
+    sum_exp, 
+    cohort,
+    color_by,
+    umap_projection,
     base_size = 10,
     point_size = 1,
     alpha_val = 0.8
@@ -35,7 +77,7 @@ plot_umap <- function(
         tibble::rownames_to_column(var = "sample_name") %>%
         dplyr::inner_join(
             .,
-            colData(dataset) %>% data.frame,
+            colData(sum_exp) %>% data.frame,
             by = "sample_name"
         )
     
@@ -44,13 +86,48 @@ plot_umap <- function(
         aes_string(x = "umap1", y = "umap2", color = color_by)
     ) + 
         ggplot2::geom_point(size = point_size, alpha = alpha_val) + 
-        ggplot2::labs(title = toupper(name_dataset)) +
+        ggplot2::labs(title = toupper(cohort)) +
         ggplot2::theme_bw(base_size = base_size)
         
 }
 
+#' Forest plot of the fitted models
+#'
+#' @param fit A model fit from survival package. 
+#' @param cohort A string. Name of cohort that will be used for title
+#'    purposes.
+#' @param names_coefficients A named list. Used for converting the name
+#'   of the coefficients to something more readable in the forest plot.
+#' @param patients A string. Specifies which patients were selected to
+#'   calculate the model fit.
+#' @param type_survival A string. Either OS (overall survival) or 
+#'   RFS (recurrence free survival).
+#' @param name_signature A string. Name of the score pathway used to 
+#'   test in the survival fit.
+#' @param path_to_save A string. Path to save the forest plot.
+#' @param width An integer. Width of the plot
+#' @param height An integer. Height of the plot
+#' @param ... Other options for the function forestplot::forestplot
+#' @examples
+#' \dontrun{
+#' forest_plot_fits(
+#'     fit,
+#'     cohort = "metabric",
+#'     names_coefficients = c(metabric = c(
+#'         "age" = "Age", "set_erpr" = "SET ER/PR"
+#'     ),
+#'     patients = "Endo Only, ER+ BC",
+#'     type_survival = "OS",
+#'     name_signature = "set_erpr",
+#'     path_to_save = "results/plots",
+#'     clip = c(0.2, 2)
+#' )
+#' }
 forest_plot_fits <- function(
-    fit, cohort, names_coefficients, patients, type_survival, ...
+    fit, cohort, names_coefficients, 
+    patients, type_survival,
+    name_signature, path_to_save, 
+    width = 6, height = 3, ...
 ){
     
     coefs_surv <- exp(coef(fit))
@@ -81,7 +158,14 @@ forest_plot_fits <- function(
         base_data
     )
     
-    output_df %>% forestplot::forestplot(
+    filename <- paste0(
+        path_to_save, "/",
+        cohort, "_", type_survival,
+        "_", name_signature, ".pdf"
+    ) 
+    
+    
+    forest_plot <- output_df %>% forestplot::forestplot(
         labeltext = c(coefficient, HR), 
         hrzl_lines = list(
             "2" = gpar(col = "#444444", columns = 1:3, lwd = 1)
@@ -105,4 +189,10 @@ forest_plot_fits <- function(
         graph.pos = 2,
         ...
     )
+    
+    pdf(file = filename, width = width, height = height)
+    plot(forest_plot)
+    dev.off()
+    
+    forest_plot
 }
